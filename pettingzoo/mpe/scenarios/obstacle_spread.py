9baae4e5a62760a8ca5bd6b4d84e775e88b8725d
Jayant <jayant.singh@uia.no>
2022-02-28 15:56:21 +0100

    two new environments

---
+++ pettingzoo/mpe/scenarios/obstacle_spread.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+from .._mpe_utils.core import Agent, Landmark, World
+from .._mpe_utils.scenario import BaseScenario
+
+
+class Scenario(BaseScenario):
+    def make_world(self, N=3, num_obstacles=2):
+        world = World()
+        # set any world properties first
+        world.dim_c = 2
+        num_agents = N
+        num_goals = N
+        world.collaborative = True
+        num_landmarks = num_goals + num_obstacles
+
+        # add agents
+        world.agents = [Agent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = f'agent_{i}'
+            agent.collide = True
+            agent.silent = True
+            agent.size = 0.15
+
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.movable = False
+            if i < num_goals:
+                landmark.name = f'goal_{i}'
+                landmark.collide = False
+                landmark.type = "goal"
+            else:
+                landmark.name = f'obstacle_{i-num_goals}'
+                landmark.collide = True
+                landmark.size = 0.3
+                landmark.boundary = False
+                landmark.type = "obstacle"
+
+        return world
+
+    def reset_world(self, world, np_random):
+        # set random initial states
+        for agent in world.agents:
+            agent.color = np.array([0.35, 0.35, 0.85])
+            agent.state.p_pos = np_random.uniform(-1, +1, world.dim_p)
+            agent.state.p_vel = np.zeros(world.dim_p)
+            agent.state.c = np.zeros(world.dim_c)
+        for landmark in self.goals(world):
+            landmark.color = np.array([0.25, 0.25, 0.25])
+            landmark.state.p_pos = np_random.uniform(-1, +1, world.dim_p)
+            landmark.state.p_vel = np.zeros(world.dim_p)
+        for landmark in self.obstacles(world):
+            landmark.color = np.array([0.85, 0.35, 0.35])
+            landmark.state.p_pos = np_random.uniform(-1, +1, world.dim_p)
+            landmark.state.p_vel = np.zeros(world.dim_p)
+
+    def obstacles(self, world):
+        return [landmark for landmark in world.landmarks if landmark.type == "obstacle"]
+
+    def goals(self, world):
+        return [landmark for landmark in world.landmarks if landmark.type == "goal"]
+
+    def benchmark_data(self, agent, world):
+        rew = 0
+        collisions = 0
+        occupied_goals = 0
+        min_dists = 0
+        for g in self.goals(world):
+            dists = [np.sqrt(np.sum(np.square(a.state.p_pos - g.state.p_pos))) for a in world.agents]
+            min_dists += min(dists)
+            rew -= min(dists)
+            if min(dists) < 0.1:
+                occupied_goals += 1
+        if agent.collide:
+            for entity in world.agents + self.obstacles(world):
+                if self.is_collision(entity, agent):
+                    rew -= 1
+                    collisions += 1
+        return (rew, collisions, min_dists, occupied_goals)
+
+    def is_collision(self, entity1, entity2):
+        delta_pos = entity1.state.p_pos - entity2.state.p_pos
+        dist = np.sqrt(np.sum(np.square(delta_pos)))
+        dist_min = entity1.size + entity2.size
+        return True if dist < dist_min else False
+
+    def reward(self, agent, world):
+        # Agents are rewarded based on minimum agent distance to each landmark, penalized for collisions
+        rew = 0
+        if agent.collide:
+            for entity in world.agents + self.obstacles(world):
+                if self.is_collision(entity, agent):
+                    rew -= 1
+        return rew
+
+    def global_reward(self, world):
+        rew = 0
+        for a in world.agents:
+            dists = [np.sqrt(np.sum(np.square(a.state.p_pos - g.state.p_pos))) for g in self.goals(world)]
+            rew -= min(dists)
+        return rew
+
+    def observation(self, agent, world):
+        # get positions of all entities in this agent's reference frame
+        entity_pos = []
+        for entity in world.landmarks:  # world.entities:
+            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+        # entity colors
+        entity_color = []
+        for entity in world.landmarks:  # world.entities:
+            entity_color.append(entity.color)
+        # communication of all other agents
+        comm = []
+        other_pos = []
+        for other in world.agents:
+            if other is agent:
+                continue
+            comm.append(other.state.c)
+            other_pos.append(other.state.p_pos - agent.state.p_pos)
+        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos + comm)
